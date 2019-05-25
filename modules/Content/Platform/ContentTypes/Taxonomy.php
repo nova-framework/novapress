39dd91572553975c0352692ab8ada0f22c55c9ac
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2019-05-26 00:12:25 +0300

    Improve the Content module

--- modules/Content/Platform/ContentTypes/Taxonomy.php
+++ modules/Content/Platform/ContentTypes/Taxonomy.php
@@ -8,9 +8,19 @@ use Modules\Content\Platform\TaxonomyTypeManager;
 
 abstract class Taxonomy extends ContentType
 {
+    /**
+     * @var bool
+     */
+    protected $unique = false;
+
 
     public function __construct(TaxonomyTypeManager $manager, array $options)
     {
         parent::__construct($manager, $options);
+    }
+
+    public function isUnique()
+    {
+        return $this->unique;
     }
 }
