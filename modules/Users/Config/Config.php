b116c97cbe390a65c7172bd89b808f642663490e
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2019-07-13 19:08:58 +0300

    Overall improvements

--- modules/Users/Config/Config.php
+++ modules/Users/Config/Config.php
@@ -22,14 +22,4 @@ return array(
             'validity' => 15, // In minutes.
         ),
     ),
-
-    'reminders' => array(
-        'messages' => array(
-            'password' => __d('users', 'Passwords must be at least six characters and match the confirmation.'),
-            'user'     => __d('users', 'We can\'t find an User with that e-mail address.'),
-            'token'    => __d('users', 'This password reset token is invalid.'),
-            'sent'     => __d('users', 'Password reminder sent!'),
-            'reset'    => __d('users', 'Password has been reset!'),
-        ),
-    ),
 );
